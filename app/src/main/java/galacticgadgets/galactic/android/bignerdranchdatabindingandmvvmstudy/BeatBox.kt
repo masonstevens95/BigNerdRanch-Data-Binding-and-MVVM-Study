2e4df578e7fe32bc0be28c83fbed407ab54a116a
Mason Stevens <mason.c.stevens@gmail.com>
2020-06-25 17:27:03 -0400

    load sounds into Beatbox (recyclerview?)

--- app/src/main/java/galacticgadgets/galactic/android/bignerdranchdatabindingandmvvmstudy/BeatBox.kt
+++ app/src/main/java/galacticgadgets/galactic/android/bignerdranchdatabindingandmvvmstudy/BeatBox.kt
@@ -1,15 +1,22 @@
 package galacticgadgets.galactic.android.bignerdranchdatabindingandmvvmstudy
 
+import android.content.res.AssetFileDescriptor
 import android.content.res.AssetManager
+import android.media.SoundPool
 import android.util.Log
+import java.io.IOException
 import java.lang.Exception
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
+private const val MAX_SOUNDS = 5
 
 class BeatBox(private val assets: AssetManager) {
 
     val sounds: List<Sound>
+    private val soundPool = SoundPool.Builder()
+        .setMaxStreams(MAX_SOUNDS)
+        .build()
 
     init{
         sounds = loadSounds()
@@ -31,8 +38,22 @@ class BeatBox(private val assets: AssetManager) {
         soundNames.forEach { filename ->
             val assetPath = "$SOUNDS_FOLDER/$filename"
             val sound = Sound(assetPath)
-            sounds.add(sound)
+            try{
+                load(sound)
+                sounds.add(sound)
+            }catch (ioe: IOException){
+                Log.e(TAG, "could not load $filename", ioe)
+            }
         }
         return sounds
     }
+
+    //SoundPool loading
+    private fun load(sound: Sound){
+        val afd: AssetFileDescriptor = assets.openFd(sound.assetPath)
+        val soundId = soundPool.load(afd, 1)
+        sound.soundId = soundId
+    }
+
+
 }
--- app/src/main/java/galacticgadgets/galactic/android/bignerdranchdatabindingandmvvmstudy/Sound.kt
+++ app/src/main/java/galacticgadgets/galactic/android/bignerdranchdatabindingandmvvmstudy/Sound.kt
@@ -3,7 +3,7 @@ package galacticgadgets.galactic.android.bignerdranchdatabindingandmvvmstudy
 
 private const val WAV = ".wav"
 
-class Sound(val assetPath: String) {
+class Sound(val assetPath: String, var soundId: Int? = null) {
 
     val name = assetPath.split("/").last().removeSuffix(WAV)
 
